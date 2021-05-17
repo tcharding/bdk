b5283f91dd4c59084a2dfc108f92b04ae56fc394
Tobin Harding <me@tobin.cc>
2021-05-18 09:31:05 +1000

    Add esplora integration test module
    In preparation for hacking on the `esplora` blockchain module add an
    integration module to run a couple of simple connection tests. Use
    Blockstreams's public Esplora instance.

---
+++ tests/esplora.rs
@@ -0,0 +1,44 @@
+//! Run some integration tests against Blockstream's public testnet Esplora instance.
+#[cfg(feature = "esplora")]
+mod tests {
+    use std::str::FromStr;
+
+    use anyhow::Result;
+
+    use bdk::blockchain::{Blockchain, EsploraBlockchain, NoopProgress};
+    use bdk::database::MemoryDatabase;
+    use bdk::wallet::Wallet;
+
+    use bitcoin::{Network, Txid};
+
+    const ESPLORA_URL: &str = "https://blockstream.info/testnet/api";
+    const DESCRIPTOR: &str = "wpkh([c258d2e4/84h/1h/0h]tpubDDYkZojQFQjht8Tm4jsS3iuEmKjTiEGjG6KnuFNKKJb5A6ZUCUZKdvLdSDWofKi4ToRCwb9poe1XdqfUnP4jaJjCB2Zwv11ZLgSbnZSNecE/0/*)";
+    const TXID: &str = "e0b603b64aae91c8fb49d452103ebd99620b38a5fb36d69c9cc801e20d1f95a6";
+
+    #[test]
+    fn can_get_tx() -> Result<()> {
+        let txid = Txid::from_str(TXID)?;
+
+        let blockchain = EsploraBlockchain::new(ESPLORA_URL, None);
+        let tx = blockchain.get_tx(&txid)?;
+
+        assert!(tx.is_some());
+
+        Ok(())
+    }
+
+    #[test]
+    fn sync_wallet() -> Result<()> {
+        let blockchain = EsploraBlockchain::new(ESPLORA_URL, None);
+        let wallet = Wallet::new(
+            DESCRIPTOR,
+            None,
+            Network::Testnet,
+            MemoryDatabase::default(),
+            blockchain,
+        )?;
+        wallet.sync(NoopProgress, None)?;
+
+        Ok(())
+    }
+}
